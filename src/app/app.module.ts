1ba3a0f5edcfb70479f28aa3ceffc058defbc0cc
Roxane Crépeaux <crepeaux.roxane@gmail.com>
2024-09-23 21:52:13 +0200

    Creating visual of home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ChartModule } from 'primeng/chart';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ChartModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,9 +10,50 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
 
+  data: any;
+  options: any;
+
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+
+    this.data = {
+        labels: ['Germany', 'United States', 'France', 'Spain', 'Itally'],
+        datasets: [
+            {
+                data: [125, 345, 113, 54, 96],
+                backgroundColor: ['#793d52', '#89a1db', '#9780a1', '#b8cbe7', '#956065'],
+                hoverBackgroundColor: ['#8d4a62', '#9cb1e0', '#a18ba8', '#c3d3eb', '#a06d74']
+
+            }
+        ]
+    };
+
+    this.options = {
+        plugins: {
+            legend: {
+                labels: {
+                    usePointStyle: true,
+                    color: '#000000'
+                }
+            },
+            tooltip: {
+                callbacks: {
+                    label: (context: any) => {
+                        const value = context.raw || 0;
+                        return [`🎖️ ${value}`];
+                    }
+                },
+                backgroundColor: '#04838f',
+                titleAlign: 'center',
+                bodyAlign: 'center',
+                bodyFont: { size: 16 },
+                titleFont: { size: 18},
+                padding: 10,
+                displayColors: false
+            }
+        }
+    };
   }
 }
